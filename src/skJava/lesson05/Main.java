4bc3ae06c311bbb7b9d3c10c6ce269ad00eb6ecd
eiko <eiko@EIKOWIN-PC>
2023-04-09 16:14:43 +0900

    三角形の面積を求める処理

--- src/skJava/lesson05/Main.java
+++ src/skJava/lesson05/Main.java
@@ -4,5 +4,20 @@ public class Main {
 
 	public static void main(String[] args) {
 		
+		//Triangle型のインスタンスを生成
+		Triangle triangle = new Triangle();
+		
+		//三角形の面積を求める
+		double tri = triangle.calc(1, 9);
+		
+		//「ln」なしは、改行なし
+		System.out.print("三角形の面積は " + tri);
+		System.out.println(" です。" );
+		//改行
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		
+		System.out.println("終わり。" );
 	}
 }
---
+++ src/skJava/lesson05/Triangle.java
@@ -0,0 +1,12 @@
+package skJava.lesson05;
+
+public class Triangle {
+
+	public double calc(int low, int height) {
+		
+		double d = low * height / 2.0;
+		
+		return d;
+	}
+
+}
